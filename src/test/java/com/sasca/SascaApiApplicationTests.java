f5f73167ebd072d1f682b2cf10524b66cd440523
sasca37 <sasca37@naver.com>
2023-04-29 15:05:37 +0900

    initial project

---
+++ src/test/java/com/sasca/SascaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SascaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
