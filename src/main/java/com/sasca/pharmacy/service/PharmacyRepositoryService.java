d917d0dc8896c0c047bb4c0cc89af6faa958356e
sasca37 <sasca37@naver.com>
2023-05-01 19:04:27 +0900

    docker mariadb init data 설정 - 공공 데이터 init insert

--- src/main/java/com/sasca/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/sasca/pharmacy/service/PharmacyRepositoryService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -28,5 +29,10 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
